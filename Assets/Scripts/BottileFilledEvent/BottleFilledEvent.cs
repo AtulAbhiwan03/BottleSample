a343a82f5ea54c49cf758bbc240e6727722f48f1
Atul <125960822+AtulAbhiwan03@users.noreply.github.com>
2024-02-09 18:29:00 +0530

    Fixes

--- Assets/Scripts/BottileFilledEvent/BottleFilledEvent.cs
+++ Assets/Scripts/BottileFilledEvent/BottleFilledEvent.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BottleFilledEvent : MonoBehaviour
 {
     public GameObject Bottle;
     public float time;
+    public TMP_Text countdown;
     public Transform capParent;
      private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -22,7 +24,15 @@ public class BottleFilledEvent : MonoBehaviour
 
     IEnumerator Reshape(float time)
     {
+        countdown.text = "3";
+        yield return new WaitForSeconds(time); 
+        countdown.text = "2";
+        yield return new WaitForSeconds(time); 
+        countdown.text = "1";
         yield return new WaitForSeconds(time);
+        countdown.text = "0";
+        yield return new WaitForSeconds(time);
+        countdown.text = "";
         Bottle.SetActive(false);
         foreach (Transform t in capParent)
         {
